06b93aaa8681ba9e2cc66cdcbf1eb0d37f88fd34
ekpenyongeetim <ekpenyongeetim@gmail.com>
2023-08-09 20:59:33 +0100

    new changes

--- src/books.js
+++ src/books.js
@@ -1,6 +1,6 @@
 import img1 from "./images/book-1.jpg";
 import img2 from "./images/book-2.jpg";
-import img3 from "./images/book-3.webp";
+import img3 from "./images/lawsofpower.jpg";
 
 export const books = [
   {
